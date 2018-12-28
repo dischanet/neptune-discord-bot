52cd861884eb0a8c8d029c2684fd2cb995475ac8
InkoHX <inkohx@gmail.com>
2018-12-29 05:03:17 +0900

    add

---
+++ commands/Image/Animal/dog.js
@@ -0,0 +1,17 @@
+const { Command } = require('klasa')
+const { MessageAttachment } = require('discord.js')
+const fetch = require('node-fetch')
+
+module.exports = class extends Command {
+  constructor (...args) {
+    super(...args, {
+      requiredPermissions: ['ATTACH_FILES', 'SEND_MESSAGES'],
+      description: language => language.get('COMMAND_DOG_DESCRIPTION')
+    })
+  }
+
+  async run (message) {
+    const file = await fetch('https://dog.ceo/api/breeds/image/random').then(res => res.json())
+    return message.send(new MessageAttachment(file['message']))
+  }
+}
