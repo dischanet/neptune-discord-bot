cc2d9c96e7f8e8d43162061c896f7c9339256aeb
InkoHX <inkohx@gmail.com>
2018-12-31 13:29:03 +0900

    Add commands

--- commands/General/Chat Bot Info/language.js
+++ commands/General/Chat Bot Info/language.js
@@ -4,7 +4,6 @@ module.exports = class extends Command {
   constructor (...args) {
     super(...args, {
       runIn: ['text'],
-      requiredPermissions: ['SEND_MESSAGES'],
       aliases: ['lang'],
       cooldown: 5,
       permissionLevel: 6,
