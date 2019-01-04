32e2e616df9dc61b5b94096b395650702a9b4440
InkoHX <inkohx@gmail.com>
2019-01-04 23:30:49 +0900

    Add poggit command

---
+++ commands/DevTool/PocketMine/poggit.js
@@ -0,0 +1,25 @@
+const { Command } = require('klasa')
+const { MessageEmbed } = require('discord.js')
+const request = require('request')
+
+module.exports = class extends Command {
+  constructor (...args) {
+    super(...args, {
+      description: '',
+      usage: '<query:str>'
+    })
+  }
+
+  async run (message, [query]) {
+    await request('https://poggit.pmmp.io/plugins.json', { method: 'GET', json: true }, (e, r, b) => {
+      const Embed = new MessageEmbed()
+        .setTitle(`検索結果: ${query}`)
+      b.filter((item, index) => {
+        if (item['name'] === query) {
+          Embed.addField(`${item['name']} v${item['version']}`, item['html_url'])
+        }
+      })
+      return message.sendEmbed(Embed)
+    })
+  }
+}
