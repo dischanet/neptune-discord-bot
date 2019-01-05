e9e38a57f9e513dcf9f8a40cc4fb1288a71b6e36
InkoHX <inkohx@gmail.com>
2019-01-05 22:20:47 +0900

    Delete

--- utils/GuildSchema/GuildDefaultSchema.js
+++
@@ -1,9 +0,0 @@
-const { KlasaClient } = require('klasa')
-
-module.exports.MinAccAge = KlasaClient.defaultGuildSchema
-  .add('minAccAge', 'integer', { default: 1800000 })
-
-module.exports.Mute = KlasaClient.defaultGuildSchema
-  .add('roles', schema => schema
-    .add('muted', 'role')
-  )
