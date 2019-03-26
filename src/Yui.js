58320ce8bcfbf560121cf4239b92fc1120e3c468
InkoHX <inkohx@gmail.com>
2019-03-26 20:26:01 +0900

    Update

--- src/Yui.js
+++ src/Yui.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 const { Client } = require('klasa')
-const { version } = require('../package.json')
 const DBLAPI = require('dblapi.js')
 
 const client = new Client({
@@ -26,16 +25,6 @@ if ('DBL_TOKEN' in process.env) {
 }
 
 client.login(process.env.DISCORD_BOT_TOKEN)
-
-setInterval(() => {
-  const heapUsed = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)
-  const heapTotal = (process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)
-  const onlineUser = client.users.filter(user => user.presence.status === 'online').size
-  const totalUser = client.users.size
-  const guilds = client.guilds.size
-  const ping = Math.round(client.ws.ping)
-  process.title = `NeptuneBot v${version} - Memory: ${heapUsed}/${heapTotal} MB | Heartbeat: ${ping}ms | Users: ${onlineUser}/${totalUser} | ${guilds} guilds`
-}, 1000)
 
 module.exports = {
   Utils: require('./lib/util/utils'),
