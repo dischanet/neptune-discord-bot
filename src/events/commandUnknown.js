6a5d31c300ba5543c6d543bd8201436041b0787c
InkoHX <inkohx@gmail.com>
2019-04-07 13:09:38 +0900

    fixes

--- src/events/commandUnknown.js
+++ src/events/commandUnknown.js
@@ -8,7 +8,7 @@ class CommandUnknown extends Event {
    * @param {int} prefixLength
    */
   run (message, commandText, prefix, prefixLength) {
-    message.sendMessage(`\`${commandText}\` は存在しないコマンドです。誤字、脱字が無いか \`yuigahama!help\` で確認して下さい。\nhttps://tenor.com/view/do-not-cry-anime-gif-7999979`)
+    message.sendMessage(`\`${commandText}\` は存在しないコマンドです。誤字、脱字が無いか \`neptune!help\` で確認して下さい。\nhttps://tenor.com/view/do-not-cry-anime-gif-7999979`)
   }
 }
 
